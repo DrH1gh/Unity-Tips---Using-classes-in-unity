8e26ef395377a7d73b6ab37945359ec78ae80b9e
DrH1gh <alexandru.n.popescu@gmail.com>
2017-06-12 18:07:53 +0300

    classes generated

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+// Notice you don't have to inherit from MonoBehaviour.
+public class Enemy
+{
+
+    private int health;
+    private int attack;
+    private string name;
+
+    //This is the constructor
+    public Enemy(int _health, int _attack, string _name)
+    {
+        this.health = _health;
+        this.attack = _attack;
+        this.name = _name;
+    }
+
+    //Some function, to print the information in the console.
+    public void EnemyInfo()
+    {
+        Debug.Log("Enemy " + this.name + " created! Health: " + this.health + " Attack power: " + this.attack);
+    }
+
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+// Notice you don't have to inherit from MonoBehaviour.
+public class Player
+{
+    private string heroName;
+    private int health;
+    private int attack;
+    private float magic;
+
+    //This is the constructor
+    public Player(string _name, int _health, int _attack, float _magic)
+    {
+        this.health = _health;
+        this.attack = _attack;
+        this.heroName = _name;
+        this.magic = _magic;
+    }
+
+    //Some function, to print the information in the console.
+    public void PlayerInfo()
+    {
+        Debug.Log("**********************************");
+        Debug.Log("Hero " + this.heroName + " created! Health: " + this.health + " Attack power: " + this.attack + " Magic power: " + this.magic);
+        Debug.Log("**********************************");
+    }
+
+}
+
